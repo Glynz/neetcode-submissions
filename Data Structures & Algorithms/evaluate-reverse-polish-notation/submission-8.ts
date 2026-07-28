8960de752933843f45346b340ad1930c65a473f5
Michael Capone <mcapone6@gatech.edu>
2026-07-28 10:38:00 -0400

    Add: evaluate-reverse-polish-notation - submission-8

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-8.ts
@@ -0,0 +1,35 @@
+class Solution {
+    /**
+     * @param {string[]} tokens
+     * @return {number}
+     */
+    evalRPN(tokens: string[]): number {
+        // Push integers into stack until operand
+        // Pop the 2 values and perform the operand
+        // Push the result into the stack
+        // Repeat and return the top value when complete
+        const stack: number[] = [];
+
+        for(const s of tokens) {
+            if (!Number.isNaN(Number(s))) {
+                stack.push(Number(s));
+            } else {
+                // while (stack.length !== 1) {
+                    let num2 = stack.pop();
+                    let num1 = stack.pop();
+                    if(s === '+') {
+                        stack.push(num1 + num2);
+                    } else if (s === '-') {
+                        stack.push(num1 - num2);
+                    } else if (s === '*') {
+                        stack.push(num1 * num2);
+                    } else if (s === '/') {
+                        stack.push(Math.trunc(num1 / num2));
+                    }
+                // }
+            }
+        }
+
+        return stack.pop();
+    }
+}
