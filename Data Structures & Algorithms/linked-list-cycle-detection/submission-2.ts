020a7980bad6cbfc6d042c3212e51797c81ebf50
Michael Capone <mcapone6@gatech.edu>
2026-08-13 16:14:56 -0400

    Add: linked-list-cycle-detection - submission-2

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-2.ts
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {ListNode} head
+     * @return {boolean}
+     */
+    hasCycleON(head: ListNode | null): boolean {
+        // Keep a map of visited nodes using the ListNode as the key
+        // If the key is already in the map then return false
+        // Iterate over the entire linked list until you reach
+        // the end
+        const visitedMap = new Map<ListNode, number>();
+        let index = 0;
+        let currentNode = head;
+        while(currentNode) {
+            if (visitedMap.has(currentNode)) return true;
+            visitedMap.set(currentNode, index++);
+            currentNode = currentNode.next;
+        }
+
+        return false;
+    }
+
+    hasCycle(head: ListNode | null): boolean {
+        let fastNode = head;
+        let slowNode = head;
+        while(fastNode && fastNode.next) {
+            slowNode = slowNode.next;
+            fastNode = fastNode.next;
+            fastNode = fastNode.next;
+            if(slowNode == fastNode) return true;
+        }
+
+        return false;
+    }
+}
